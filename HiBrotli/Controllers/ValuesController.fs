a85bf78aea56a04cfd5444baa380b96be7e76d0d
wk-j <somnuk.wk@gmail.com>
2018-03-16 23:03:09 +0700

    Initial commit

---
+++ HiBrotli/Controllers/ValuesController.fs
@@ -0,0 +1,32 @@
+﻿namespace HiBrotli.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get() =
+        [|"value1"; "value2"|]
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        "value"
+
+    [<HttpPost>]
+    member this.Post([<FromBody>] value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>] value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
